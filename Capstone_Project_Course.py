00217a71b84c79aa551c1f7b574936001b02e094
truonghuynhphat-art <82090302+truonghuynhphat-art@users.noreply.github.com>
2021-04-29 08:55:37 +0700

    Add files via upload

---
+++ Capstone_Project_Course.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction
+# #### This notebook will be used to prepare the capstone project pertaining the course 'Applied Data Science Capstone' by IBM.
+
+# In[2]:
+
+
+import pandas as pd
+import numpy  as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
